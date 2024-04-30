1b05cdc14c5e4fbfb4d27aa51b57646e7b3cdf51
Pop2323 <omar4500455@gmail.com>
2024-04-30 02:24:38 -0700

    frontendForFun

---
+++ markdown2html.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <Markdown_file> <Output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+sys.exit(0)
